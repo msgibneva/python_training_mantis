392ccb3152591a52195964915f3c00d820a54eaa
Mari Sgibneva <mari.sgibnev@gmail.com>
2020-02-07 22:23:18 +0300

    фиксы разметки/имен тестов удаления/добавления проектов

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -4,8 +4,7 @@ from model.project import Project
 def test_add_project(app):
     app.session.login("administrator", "root")
     old_project_list = app.project.get_project_list()
-    print(old_project_list)
-    new_project = Project(name="project")
+    new_project = Project(name="project2")
     app.project.create(new_project)
     new_project_list = app.project.get_project_list()
     old_project_list.append(new_project)
